e8e4af6cefe064f41e3bcd6f931c2a62c14e4a2e
CrinitusFeles <crinitusfeles@gmail.com>
2026-05-02 15:13:45 +0700

    bump version

--- qissuereporter/__init__.py
+++ qissuereporter/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
-from .creator.main_window import ReporterWindow
-from .viewer.main_window import ViewerWindow
+from .creator.main_window import ReporterWindow  # noqa: F401
+from .viewer.main_window import ViewerWindow  # noqa: F401
